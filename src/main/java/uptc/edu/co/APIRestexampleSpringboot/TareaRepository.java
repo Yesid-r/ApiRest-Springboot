12fd4068abc8c3080a485c7eb570509214a28f6a
Yesid-r <dairo.rincon@uptc.edu.co>
2023-03-20 11:41:53 -0500

    add crossOrigin

--- src/main/java/uptc/edu/co/APIRestexampleSpringboot/TareaRepository.java
+++ src/main/java/uptc/edu/co/APIRestexampleSpringboot/TareaRepository.java
@@ -3,7 +3,9 @@ package uptc.edu.co.APIRestexampleSpringboot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin()
 @Repository
 @RepositoryRestResource(path = "tareas", collectionResourceRel = "tareas")
 public interface TareaRepository extends JpaRepository<Tarea, Integer> {
